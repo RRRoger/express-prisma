50cf4d955e6000b36b583759ca12650870230a9b
rrroger <chenpeng.0418@163.com>
2022-06-11 11:37:56 +0800

    [FEAT] add curd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/users', async function(req, res, next) {
+  const rows = await prisma.user.findMany({
+  })
+  res.json(rows)
+});
+
+router.get('/models', async function(req, res, next) {
+  const rows = await prisma.model.findMany({
+    include: {
+      ModelLine: true
+    }
+  })
+  res.json(rows)
 });
 
 module.exports = router;
